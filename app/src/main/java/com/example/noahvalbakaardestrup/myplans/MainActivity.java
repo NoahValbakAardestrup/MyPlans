17a12e107a162d4fc6d2d4775017157cc851b480
Kennet Kristensen <kkr@trackunit.com>
2018-11-22 11:10:58 +0100

    f added

--- app/src/main/java/com/example/noahvalbakaardestrup/myplans/MainActivity.java
+++ app/src/main/java/com/example/noahvalbakaardestrup/myplans/MainActivity.java
@@ -8,18 +8,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textTitle = findViewById(R.id.textTitle);
-        Button buttonCreate = findViewById(R.id.buttonCreate);
+        textTitle = findViewById(R.id.textTitle);
+            Button buttonCreate = findViewById(R.id.buttonCreate);
         buttonCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textTitle.setText("YO");
-            }
+                @Override
+                public void onClick(View v) {
+                    textTitle.setText("YO");
+
+                    int val = 4;
+                    textTitle.setText(String.valueOf(val));
+
+                    f();
+                }
         });
+    }
+
+
+    private void f(){
+        String textval = "oi oi";
+        textTitle.setText(textval);
     }
 }
