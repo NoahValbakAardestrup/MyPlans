cc49437cc73f368113fe644968f61cc379dfde66
Kennet Kristensen <kkr@trackunit.com>
2018-11-22 10:51:40 +0100

    added button

--- app/src/main/java/com/example/noahvalbakaardestrup/myplans/MainActivity.java
+++ app/src/main/java/com/example/noahvalbakaardestrup/myplans/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.noahvalbakaardestrup.myplans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textTitle = findViewById(R.id.textTitle);
+        Button buttonCreate = findViewById(R.id.buttonCreate);
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textTitle.setText("YO");
+            }
+        });
     }
 }
