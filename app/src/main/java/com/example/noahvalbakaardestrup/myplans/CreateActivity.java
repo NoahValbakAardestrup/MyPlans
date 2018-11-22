961dcc2a6196500e758a052964f351d994967258
Kennet Kristensen <kkr@trackunit.com>
2018-11-22 13:28:26 +0100

    create seek

--- app/src/main/java/com/example/noahvalbakaardestrup/myplans/CreateActivity.java
+++ app/src/main/java/com/example/noahvalbakaardestrup/myplans/CreateActivity.java
@@ -2,12 +2,38 @@ package com.example.noahvalbakaardestrup.myplans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class CreateActivity extends AppCompatActivity {
+
+    SeekBar seekbar;
+    TextView importance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+
+        seekbar = findViewById(R.id.seekBar);
+        importance = findViewById(R.id.importance);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                importance.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
